1c17f5d4d240890bd01c455aa811437fb86d9698
Oleh Cheliakh <oleh.cheliakh@gmail.com>
2024-02-02 23:13:06 +0200

    Finish 3 tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,29 @@
+class Key {
+	private signature: number = Math.random();
+
+	getSignature(): number {
+		return this.signature;
+	}
+}
+
+class Person {
+	constructor(private key: Key) {
+		this.key = key;
+	}
+
+	getKey(): number {
+		return this.key.getSignature();
+	}
+}
+
+abstract class House {
+	door: boolean;
+	key: Key;
+	constructor(key: Key) {
+		this.key = key;
+	}
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +32,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
